53650561526151a55c3dc6ed4e329caf7aa5b0ab
limpidblackeye <limipid.eye@gmail.com>
2018-04-09 22:00:46 +0800

    Update cell_seperate_file.sh

--- cell_seperate_file.sh
+++ cell_seperate_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir_data="/Users/april/Desktop/2018Spring/cell_nucleus/data/"
+dir_data="/Users/april/Desktop/2018Spring/cell_nucleus/data/test_"
 dir_gray=$dir_data"stage1_train_gray"
 dir_color=$dir_data"stage1_train_color"
 list_gray=$dir_data"dir_of_gray.list"
@@ -10,4 +10,10 @@ for name in `cat $list_color`
 do
 	echo $name
 	cp -r $name $dir_color
+done
+
+for name in `cat $list_gray`
+do
+	echo $name
+	cp -r $name $dir_gray
 done
